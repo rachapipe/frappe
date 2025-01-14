d634e29094169361bffb1bee6595accbedaf2ff5
ruthra kumar <ruthra@erpnext.com>
2025-01-14 10:49:32 +0530

    refactor: make scheduled_against mandatory
    (cherry picked from commit 4ab7c103c176fc8d391fc3f71528d8b9c8262d04)
    
    # Conflicts:
    #	frappe/core/doctype/scheduler_event/scheduler_event.json
    #	frappe/website/doctype/marketing_campaign/marketing_campaign.py

--- frappe/website/doctype/marketing_campaign/marketing_campaign.py
+++ frappe/website/doctype/marketing_campaign/marketing_campaign.py
@@ -14,6 +14,11 @@ class MarketingCampaign(Document):
 	if TYPE_CHECKING:
 		from frappe.types import DF
 
+<<<<<<< HEAD:frappe/website/doctype/marketing_campaign/marketing_campaign.py
 		campaign_description: DF.SmallText | None
+=======
+		method: DF.Data | None
+		scheduled_against: DF.Link
+>>>>>>> 4ab7c103c1 (refactor: make scheduled_against mandatory):frappe/core/doctype/scheduler_event/scheduler_event.py
 	# end: auto-generated types
 	pass
